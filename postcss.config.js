29c2c0941bc4b8bdf86dd483177d3f4fba64537f
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-08-14 19:13:23 -0400

    estrutura html finalizada

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 const atImport = require('postcss-import')
 const cssNano = require('cssnano')
+const postCssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   plugins: [
     atImport,
+    postCssPresetEnv({stage: 2}),
     cssNano
   ]
 }
